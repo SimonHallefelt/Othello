88844840f598f7cc74733869b1a826c789594c72
Simon Hallefält <simon@hallefelt.se>
2025-04-11 17:20:16 +0200

    added flags to look for memory leaks using the debugger

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "../include/Game.hpp"
+#include <crtdbg.h> // look for memory leaks using vs code
 
 int main() {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // look for memory leaks using vs code debugger
+    
     startGame();
     return 0;
 }
