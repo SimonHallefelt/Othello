76778948129ebce560cc92d1cbc4da373b8fa6bd
Simon hallefält <simon@hallefelt.se>
2025-06-26 21:42:49 +0200

    wrapped the windows specific code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <memory>
-#include <crtdbg.h> // look for memory leaks using vs code
+#ifdef _WIN32 // Windows only
+    #include <crtdbg.h> // look for memory leaks using vs code
+#endif
 
 extern void startGUI();
 
 int main() {
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // look for memory leaks using vs code debugger
-    
+    #ifdef _WIN32 // Windows only
+        _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // look for memory leaks using vs code debugger
+    #endif
+
     startGUI();
 
     return 0;
